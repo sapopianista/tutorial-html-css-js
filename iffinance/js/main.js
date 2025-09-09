86e6d05db6bbf0199aa89968c0c8d8e6eee97374
Tiago Vieira <tiagoferreiravieira167@gmail.com>
2025-09-09 19:24:36 -0300

    correcoes

--- iffinance/js/main.js
+++ iffinance/js/main.js
@@ -9,40 +9,31 @@ function closeModal(){
 }
 
 function addTicker(event){
-    event.preventDefault()
+   event.preventDefault()
 
-        const ticker = event.target.ticker.value
-        const bolsa = event.target.bolsa.value
-        const valor = event.target.valor.value
-        const ativos = event.target.ativos.value
+    const ticker = event.target.ticker.value
+    const bolsa = event.target.bolsa.value
+    const valor = event.target.valor.value
+    const ativos = event.target.ativos.value
 
-        const total = valor * ativos
+    const total = valor * ativos
 
-        const card = `
-            <div class="card">
-                        <div class="card-header">
-                            <div class="card-logo">
-
-                                <h2 class="ticker">
-                                    <img
-                                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/500px-Apple_logo_black.svg.png">${ticker}
-                                </h2>
-                            </div>
-                            <h2 class="bolsa">${bolsa}</h2>
-                        </div>
-                        <div class="card-main">
-                            <h1 class="preço">▲ U${valor}</h1>
-                        </div>
-                        <div class="card-footer">
-                            <h3 class="ativos">N° ativos: ${ativos}</h3>
-                            <h3 class="posição">U$${total}</h3>
-                        </div>
-                    </div>
-        `
-        const cards = document.getElementById("cards")
-
-        cards.innerHTML += card
-        closeModal()
-
-        console.log(card)
+    const card = `
+        <div class="card-stock">
+            <header>
+                <h2>${ticker}</h2>
+                <h3>${bolsa}</h3>
+            </header>
+            <main>
+                <span>▲ US$ ${valor}</span>
+            </main>
+            <footer>
+                <span>Ativos: <span>${ativos}</span></span>
+                <span>US$ ${total}</span>
+            </footer>
+        </div>
+    `
+    const cards = document.querySelector("#cards")
+    cards.innerHTML += card
+    closeModal()
 }
